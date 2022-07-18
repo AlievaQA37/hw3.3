4bd14b1c31ff6898ec23c7bcb9a31b83222a336b
Chloe Alieva <chloe@email.com>
2022-07-18 14:14:23 +0300

    Кредитный калькулятор v2.0

--- CreditPaymentService.java
+++ CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
     public double calculate(double p, double m, double s) {
-        double i = p / (12 *100); // проценты в месяц
+        double i = p / (12 * 100); // проценты в месяц
         double a = Math.pow(1 + i, m); // возведение в степень
-        double k = (i * a)/(a-1);
+        double k = (i * a) / (a - 1);
         double e = s * k;
         return e;
     }
